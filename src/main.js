c39f68c9ff8cb3c635018bd9a79123b0c36a8980
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-03 18:19:26 -0300

    add http lib handler axios

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import axios from './plugins/axios';
 import '@babel/polyfill';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
