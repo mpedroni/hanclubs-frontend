1c3a503cdb9ba9cacb8d3b2eb2ce413e32dd1a19
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-03 16:42:19 -0300

    create Home view

---
+++ src/views/Home/components/Menu.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card>
+    <v-app-bar color="accent" flat>
+      <v-icon v-if="icon" left color="grey--text text--darken-2">
+        {{ icon }}
+      </v-icon>
+
+      <v-toolbar-title class="grey--text text--darken-2" :title="title">
+        {{ title }}
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-list>
+      <v-list-item v-for="option in actions" :key="option.to" :to="option.to">
+        <v-list-item-icon>
+          <v-icon color="primary">{{ option.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>
+          {{ option.title }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+
+    actions: {
+      type: Array[Object],
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-row>
+    <v-col v-for="{ title, icon, actions } in menus" :key="title" cols="12" md="6">
+      <home-menu :title="title" :icon="icon" :actions="actions" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import HomeMenu from './components/Menu';
+
+export default {
+  name: 'Home',
+  components: {
+    HomeMenu,
+  },
+  data: () => ({
+    menus: [
+      {
+        title: 'Times de Futebol',
+        icon: 'mdi-soccer',
+        actions: [
+          {
+            title: 'Cadastrar um novo clube',
+            to: '/teams/new',
+            icon: 'mdi-plus',
+          },
+          {
+            title: 'Ver clubes cadastrados',
+            to: '/teams',
+            icon: 'mdi-clipboard-list',
+          },
+        ],
+      },
+      {
+        title: 'Sócios',
+        icon: 'mdi-account-tie',
+        actions: [
+          {
+            title: 'Cadastrar um novo sócio',
+            to: '/partners/new',
+            icon: 'mdi-account-plus',
+          },
+          {
+            title: 'Ver sócios cadastrados',
+            to: '/partners',
+            icon: 'mdi-account-group',
+          },
+        ],
+      },
+    ],
+  }),
+};
+</script>
