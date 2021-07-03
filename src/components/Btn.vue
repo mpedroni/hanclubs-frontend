b51968ec7480ff3aba322bfa33fe966f975b4077
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-03 18:20:21 -0300

    create the default button app component

---
+++ src/components/Btn.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn v-bind="$attrs" v-on="$listeners" outlined :color="color">
+    <v-icon left>{{ icon }}</v-icon>
+    {{ label }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'Btn',
+
+  props: {
+    color: {
+      type: String,
+      default: 'primary',
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
