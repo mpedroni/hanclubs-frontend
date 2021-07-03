4c2a45cc029630ffc96ef5ba8fa6042b7c391e17
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-03 18:20:59 -0300

    create the default snackbar app component

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-snackbar
+    v-model="snackbar.open"
+    :color="snackbar.color"
+    :timeout="snackbar.timeout || 3000"
+    top
+    right
+  >
+    {{ snackbar.message }}
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'Snackbar',
+
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    snackbar: {
+      get() {
+        return this.value;
+      },
+      set(snackbar) {
+        this.$emit('input', snackbar);
+      },
+    },
+  },
+};
+</script>
