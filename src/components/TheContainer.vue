10ca6ff73450e889f52552b42016e8ade1c67825
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-03 18:21:31 -0300

    create the default view's wrapper component

---
+++ src/components/TheContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-card outlined>
+      <v-card-title class="accent--text">
+        <v-icon color="accent"> mdi-chevron-right </v-icon>
+        {{ $route.name }}
+      </v-card-title>
+
+      <v-divider class="mx-5" />
+
+      <v-card-text>
+        <slot />
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TheContainer',
+};
+</script>
