5c308a8fb7ed2299bb522787795df4547a74fcaf
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-03 22:27:08 -0300

    create default Autocomplete component

---
+++ src/components/Autocomplete.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-autocomplete v-model="autocomplete" v-bind="$attrs" outlined />
+</template>
+
+<script>
+export default {
+  name: 'Autocomplete',
+
+  props: {
+    value: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+    autocomplete: {
+      get() {
+        return this.value;
+      },
+      set(items) {
+        this.$emit('input', items);
+      },
+    },
+  },
+};
+</script>
