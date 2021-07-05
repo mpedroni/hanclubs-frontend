f06969b6594fdaf7856a549cef155c47428cd835
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-05 05:31:07 -0300

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'import/extensions': 'off',
+    'no-shadow': 'off',
   },
 };
