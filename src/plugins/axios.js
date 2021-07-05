f74ea74667ae36612b106b8e27e9fa45acf180fc
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-05 01:25:11 -0300

    craate an handle for api response

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
 const server = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.VUE_APP_BASE_URL,
 });
+
+server.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    throw new Error(error.response.data.message).message;
+  }
+);
 
 export default server;
