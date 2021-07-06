15d2be769fa299e3878a1a1844c7c111e73e3f02
Matheus Pedroni <mpedroni77@gmail.com>
2021-07-06 00:03:33 -0300

    remove unused vuex dependence

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import vuetify from './plugins/vuetify';
 import axios from './plugins/axios';
 import '@babel/polyfill';
@@ -12,7 +11,6 @@ Vue.prototype.$http = axios;
 
 new Vue({
   router,
-  store,
   vuetify,
   render: (h) => h(App),
 }).$mount('#app');
